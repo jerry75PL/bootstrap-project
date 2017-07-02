4d4ed02dca608c8ff67ea7253cbccd2e8b643f9c
jteklak <jteklak@gmail.com>
2017-07-02 12:50:33 +0200

    jQuery-scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function(){
-   
+$(function () {
+
+
+    $(window).scroll(function () {
+        
+            
+        if ($(window).scrollTop() >= 30) {                  //to jest w praktyce to samo co niżej if ((window.scrollY)
+            $('#main-nav').addClass('nav-scroll')
+        } else {
+            $('#main-nav').removeClass('nav-scroll')
+        }
+    });
+
+    if ((window.scrollY) >= 30) {
+        $('#main-nav').addClass('nav-scroll');              //to jest w praktyce to samo co wyżej if ($(window).scrollTop()
+    }
+
+
 });
